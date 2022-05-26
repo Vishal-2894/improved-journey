70a8d7138d2a8621550cbaf61270de1e87b66119
Vishal Mahendrasing Sisodiya <105846247+Vishal-2894@users.noreply.github.com>
2022-05-26 22:59:10 +0530

    Add files via upload

---
+++ assignment-1.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+s = "this is my first python progamming class and i am learNING python string and its function"
+
+Q1.Try to extract data from index one to index 300 witha jump of 3
+# In[9]:
+
+
+s[1:300:3]
+
+
+# Q2.Try to reverse a string without using reverse function
+
+# In[11]:
+
+
+s[::-1]
+
+
+# Q3.Try to split a string after conversion of entire string in uppercase
+
+# In[13]:
+
+
+s.split()
+
+
+# In[17]:
+
+
+s.upper()
+
+
+# Q4.Try to convert whole string into lower case
+
+# In[19]:
+
+
+s.lower()
+
+
+# In[10]:
+
+
+s= "this is my first python progamming class and i am learning python string and its function"
+
+
+# Q5.Try to capaitalize the whole string
+
+# In[11]:
+
+
+s.swapcase()
+
+
+# Q6.Write a differcenc betwenn isalnum() and isalpha()
+
+# isalpha -: is TRUE when if all characters in the string are alphabets.
+# isalnum -: is TRUE when if characters in the strig are alphanumeric.
+
+# Q7.Try to give an example of expand tab
+
+# In[10]:
+
+
+s1="vishal\t12345\tsisodiya"
+
+
+# In[11]:
+
+
+s1.expandtabs()
+
+
+# Q8.Give an example of strip, lstrip and restrip
+
+# In[24]:
+
+
+s2= "   exa  mple  "
+
+
+# In[25]:
+
+
+s2.strip()
+
+
+# In[26]:
+
+
+s2.lstrip()
+
+
+# In[27]:
+
+
+s2.rstrip()
+
+
+# Q9.try to give a defination of string center function with and example
+
+# In[32]:
+
+
+a="vishal"
+
+
+# center string-: return a new string padded with the specified character. 
+# 
+
+# In[34]:
+
+
+a.center(10,'#')
+
+
+# Q10.Replace a string character by another character by takingyour own example
+
+# In[36]:
+
+
+b="sudhanshu"
+
+
+# In[38]:
+
+
+b.replace("sudhanshu","vishal")
+
+Q11.Write your own definition of complier and interpretor
+# complier & interpretor -:  is a special program language that translate the programming language into a machine code 
+
+# Q12.Python is a interpreted of compiled languagegive a clear answer with your understanding
+
+# Python is a programming language can generate the programme by Source code this source code is compiled into a binary code but 
+# this binanry code not run into the machine. this binary code run into python vertiual machine that is interpreted run the programme by reading binary code
+
+# Q13.Write a usecase of python
+Ans- Data Science , Retail Banking , business service , Machine Learning and AI
+# In[ ]:
+
+
+
+
